c0318a9be563ee2aae1b911c3fbcde09875799a3
merwe70 <58660978+merwe70@users.noreply.github.com>
2021-01-23 15:03:42 +0300

    Add project files.

---
+++ ClassMetotDemo/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class Client
+    {
+        public Client Id { get; set; }
+        public Client Tc { get; set; }
+        public Client Name { get; set; }
+        public Client Surname { get; set; }
+        public Client Position { get; set; }
+    }
+}
---
+++ ClassMetotDemo/ClientManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class ClientManager
+    {
+        public void Add(Client client)
+        {
+            Console.WriteLine(client.Id+ "Eklendi");
+        }
+
+        public void Clear(Client client)
+        {
+            Console.WriteLine(client.Id+"Silindi");
+        }
+
+        public void Update(Client client)
+        {
+            Console.WriteLine(client.Id+"Güncellendi");
+        }
+
+    }
+}
